908ba2f4b6f06535bc4739600fff5631cb73fc30
janescu8-cs <janescu8.cs@gmail.com>
2025-03-19 16:02:06 +0800

    Update mac_cracks_the_code.py

--- mac_cracks_the_code.py
+++ mac_cracks_the_code.py
@@ -3,4 +3,7 @@ word_data = {
     "c/o": ("經由; 透過（用於郵寄地址）", "Send the package to John Smith, c/o ABC Company."),
     "Smooshed": ("壓扁; 擠壓; 混合", "The sandwich got all smooshed in my bag."),
     "Bicep": ("肱二頭肌", "He flexed his bicep to show off his strength."),
+    "Safe": ("保險箱; 安全的", "She kept her valuables in a safe."),
+    "Spiky": ("尖的; 多刺的; 易怒的", "His spiky hair made him stand out in the crowd."),
+    "Gel": ("凝膠; 凝固; 融洽", "He used hair gel to style his hair."),
 }
