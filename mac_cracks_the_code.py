5496bfbe0692ec80fd0fc8ad7f7973eeca049c4d
janescu8-cs <janescu8.cs@gmail.com>
2025-03-22 19:26:20 +0800

    Update mac_cracks_the_code.py

--- mac_cracks_the_code.py
+++ mac_cracks_the_code.py
@@ -1,6 +1,5 @@
 word_data = {
     "Panache": ("派頭; 瀟灑; 神氣十足", "She always dresses with panache."),
-    "c/o": ("經由; 透過（用於郵寄地址）", "Send the package to John Smith, c/o ABC Company."),
     "Smooshed": ("壓扁; 擠壓; 混合", "The sandwich got all smooshed in my bag."),
     "Bicep": ("肱二頭肌", "He flexed his bicep to show off his strength."),
     "Safe": ("保險箱; 安全的", "She kept her valuables in a safe."),
