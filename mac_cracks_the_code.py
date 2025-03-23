37a097f8faba5b25d039cfd63d8572fc5223985e
janescu8-cs <janescu8.cs@gmail.com>
2025-03-23 15:38:12 +0800

    Update mac_cracks_the_code.py

--- mac_cracks_the_code.py
+++ mac_cracks_the_code.py
@@ -5,4 +5,9 @@ word_data = {
     "Safe": ("保險箱; 安全的", "She kept her valuables in a safe."),
     "Spiky": ("尖的; 多刺的; 易怒的", "His spiky hair made him stand out in the crowd."),
     "Gel": ("凝膠; 凝固; 融洽", "He used hair gel to style his hair."),
+    "Corn nut": ("炸玉米粒（脆脆的零食）", "She grabbed a handful of corn nuts for the road trip."),
+    "Licorice": ("甘草糖（常見於黑色或紅色的軟糖）", "Not everyone likes the taste of black licorice."),
+    "Jack up": ("抬高（價格、數量等）；提高；使增加", "The store jacked up the prices before the holiday."),
+    "Dr Pepper": ("胡椒博士汽水（美國品牌的碳酸飲料）", "I ordered a burger and a cold Dr Pepper."),
+    "Clutch": ("手拿包，小型時尚手提包（女生用）", "She carried a sleek black clutch to the party."),
 }
